fb90fce978324a01f31d3e77195d9da038608361
Inkwizzz <juromskij.vlad@mail.ri>
2023-11-16 08:54:42 +0300

    new upd

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ area.oninput = () =>
 { 
   localStorage.setItem('area', area.value) 
 };
+
+let isDarkMode = false;
+function changeColor() {
+  if (isDarkMode) {
+    document.body.classList.remove('dark-mode');
+    isDarkMode = false;
+  } else {
+    document.body.classList.add('dark-mode');
+    isDarkMode = true;
+  }
+}
